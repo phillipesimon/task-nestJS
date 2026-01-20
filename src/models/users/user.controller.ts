064f08f169f6dac35fb92383811f776740086137
Simon <phillipe.simon@gmail.com>
2026-01-20 17:12:42 -0300

    Working with Pipes

--- src/models/users/user.controller.ts
+++ src/models/users/user.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { CreateUserUseCase } from './useCases/create-user.usecase';
 import type { CreateUserDTO } from './dto/user.dto';
+import { CreateUserValidationPipe } from './pipe/create-user.validation.pipe';
 
 @Controller('/users')
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @Post()
+  @UsePipes(new CreateUserValidationPipe())
   async create(@Body() data: CreateUserDTO) {
     return await this.createUserUseCase.execute(data);
   }
