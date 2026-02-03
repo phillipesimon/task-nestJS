fcbfa3ea5aae1da867424e50a2b6255f5ab880e2
Simon <phillipe.simon@gmail.com>
2026-02-03 13:41:35 -0300

    implementing unit tests

---
+++ src/models/users/repositories/in-memory/user-in-memory.repository.ts
@@ -0,0 +1,40 @@
+import { randomUUID } from 'crypto';
+import {
+  CreateUserDTO,
+  UserCreatedDTO,
+  UsernameAndEmail,
+} from '../../dto/user.dto';
+import { IUserRepository } from '../user.repository';
+
+export class UserInMemoryRepository implements IUserRepository {
+  users: UserCreatedDTO[] = [];
+
+  async findByUsernameOrEmail(
+    data: UsernameAndEmail,
+  ): Promise<UserCreatedDTO | null> {
+    const findUser = this.users.find(
+      (user) => user.username === data.username || user.email === data.email,
+    );
+    return findUser ?? null;
+  }
+
+  async save(data: CreateUserDTO): Promise<UserCreatedDTO> {
+    const user: UserCreatedDTO = {
+      ...data,
+      id: randomUUID(),
+      createdAt: new Date(),
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  findById(id: string): Promise<UserCreatedDTO | null> {
+    throw new Error('');
+  }
+  findByUsername(data: string): Promise<UserCreatedDTO | null> {
+    throw new Error('');
+  }
+  uploadAvatar(id: string, path: string): Promise<void> {
+    throw new Error('');
+  }
+}
